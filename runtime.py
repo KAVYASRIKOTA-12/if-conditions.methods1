db0bed130ecde165dc5d2b281e668cf824b36b81
KAVYASRIKOTA-12 <Kavyasrikota989@gmail.com>
2026-06-11 14:44:21 +0530

    Update runtime.py

--- runtime.py
+++ runtime.py
@@ -1,22 +1,24 @@
 
 #if-elif-else conditions by using comparision operators
-'''a=10
+a=10
 b=20
 if a<b:
     print("less")
 elif b>a:
     print("greater")
 else:
-    print("false")'''
-'''a=10
+    print("false")
+    
+a=10
 b=20
 if a>b:
     print("less")
 elif b>a:
     print("greater")
 else:
-    print("false")'''
-'''a=10
+    print("false")
+    
+a=10
 b=20
 if a<=b:
     print("less")
@@ -27,55 +29,61 @@ elif a!=b:
 elif a==b:
     print("equal")
 else:
-    print("false")'''
+    print("false")
+    
 #if-elif-else conditions by using identify operators
-'''a=70
+a=70
 if type(a) is int:
     print("it is int")
 elif type(a) is not int:
     print("not int")
 else:
-    print("false")'''
-'''a="kavya"
+    print("false")
+    
+a="kavya"
 if type(a) is int:
     print("it is int")
 elif type(a) is not int:
     print("not int")
 else:
-    print("false")'''
+    print("false")
+    
 #if-elif-else conditions by using membership operators
-'''a=[10,20,30,40,50]
+a=[10,20,30,40,50]
 if 50 in a:
     print("true")
 elif 60 in a:
     print("no")
 else:
-    print("false")'''
-'''a=[10,20,30,40,50]
+    print("false")
+    
+a=[10,20,30,40,50]
 if 60 in a:
     print("true")
 elif 50 in a:
     print("no")
 else:
-    print("false")'''
+    print("false")
+    
 #if-elif-else conditions by using logical operators
-'''a=50
+a=50
 b=60
 if a>b and b<a:
     print("true")
 elif a<b or b!=a:
     print("yes")
 else:
-    print("false")'''
+    print("false")
 
 #multiple if condition by using comparison operators
-'''a=3
+a=3
 b=5
 if a<b:
     print("true")
 if b>a:
-    print("greater")'''
-'''a=3
+    print("greater")
+    
+a=3
 b=5
 if a<=b:
     print("true")
@@ -84,8 +92,9 @@ if b>=a:
 if b!=a:
     print("not equal")
 if a==b:
-    print("equal")'''
-'''a=int(input("a value"))
+    print("equal")
+    
+a=int(input("a value"))
 b=int(input("b value"))
 if a<b:
     print("true")
@@ -94,32 +103,35 @@ if b>a:
 if a!=b:
     print("not equal")
 if a==b:
-    print("equal")'''
+    print("equal")
+    
 #multiple if condition by using logical operators
-'''a=3
+a=3
 b=5
 if a<b and b>a:
     print("true")
 if b>=a or a==b:
     print("false")
 if b==a not a!=b:
-    print("not equal")'''
+    print("not equal")
+    
 #multiple if condition by using membership operators
-'''a=[10,30,40,60]
+a=[10,30,40,60]
 if 40 in a:
     print("true")
 if 50 not in a:
-    print("false")'''
+    print("false")
+    
 #multiple if condition by using identity operators
-'''a=59
+a=59
 if type(a) is int:
     print("true")
 if type(a) is not int:
-    print("not int")'''
+    print("not int")
 
 
 #nested-if conition by using comparison operators
-'''a=6
+a=6
 b=8
 if a<b:
     print("true")
@@ -128,8 +140,8 @@ if a<b:
     elif a==b:
          print("not equal")
 else:
-    print("false")'''
-'''a=6
+    print("false")
+a=6
 b=8
 if a>b:
     print("true")
@@ -138,8 +150,9 @@ if a>b:
     elif a==b:
          print("not equal")
 else:
-    print("false")'''
-'''a=6
+    print("false")
+    
+a=6
 b=8
 if a<=b:
     print("true")
@@ -150,9 +163,10 @@ if a<=b:
     else:
         print("equal")
 else:
-    print("false")'''
+    print("false")
+    
 #nested-if condition by using logical operators
-'''a=20
+a=20
 b=40
 if a>b and b<a:
     print("true")
@@ -161,6 +175,6 @@ if a>b and b<a:
     elif a!=b not a==b:
         print("not equal")
 else:
-    print("false")'''
+    print("false")
 
 
